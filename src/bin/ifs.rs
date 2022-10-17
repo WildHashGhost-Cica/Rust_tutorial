57842a674740add6183912eda2c6bd6f3db3fe6a
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-10-17 17:28:56 +0100

    if

---
+++ src/bin/ifs.rs
@@ -0,0 +1,18 @@
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufRead, BufReader, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
+fn main(){
+    let age: i32 = 8;
+    if (age >= 1) && (age <= 18 ){
+        println!("Important Birthday");
+    }else if ( age == 21) || (age == 50){
+        println!("Important Birthday");
+    }else if age >= 65 {
+        println!("Important Birthday");
+    }else{
+        println!("Not an Important Birtday");
+    }
+}
