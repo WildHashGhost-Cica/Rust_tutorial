24cb172bfbe045ec5450c20d428af02258bf9310
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-10-17 13:37:40 +0100

    integers

---
+++ src/bin/integers.rs
@@ -0,0 +1,14 @@
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufRead, BufReader, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
+fn main(){
+    println!("Max u32: {}", u32::MAX);
+    println!("Max u64: {}", u64::MAX);
+    println!("Max usize: {}", usize::MAX);
+    println!("Max isize: {}", isize::MAX);
+    println!("Max f32: {}", f32::MAX);
+    println!("Max i32: {}", i32::MAX);
+}
