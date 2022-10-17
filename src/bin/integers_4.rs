10ea570d16f19fb96809636ae6f406d6b944e679
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-10-17 17:22:19 +0100

    random number

---
+++ src/bin/integers_4.rs
@@ -0,0 +1,10 @@
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufRead, BufReader, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
+fn main(){
+    let random_num = rand::thread_rng().gen_range(1..101);
+    println!("Random: {}", random_num);
+}
