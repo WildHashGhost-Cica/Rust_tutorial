fbbe28f8fa6e0dca261a479e4e4696c2979ee7a0
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-10-19 11:49:05 +0100

    functions

---
+++ src/bin/functions.rs
@@ -0,0 +1,13 @@
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufRead, BufReader, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
+fn say_hello(){
+    println!("Hello WildhashGhost!");
+}
+
+fn main(){
+    say_hello();
+}
