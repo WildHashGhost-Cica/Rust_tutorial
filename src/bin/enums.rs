b001fe95ec9c01316a3bce0e686c0be7cf41636a
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-10-19 10:43:26 +0100

    enums

---
+++ src/bin/enums.rs
@@ -0,0 +1,38 @@
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufRead, BufReader, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
+fn main(){
+    enum Day{
+        Monday, 
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday,
+    }
+
+
+    impl Day {
+        fn is_weekend(&self) -> bool{
+            match self {
+                Day:: Saturday | Day::Sunday => true,
+                _ => false
+            }
+        }
+    }
+    let today:Day = Day::Monday;
+    match today {
+        Day::Monday => println!("Everyone hates Monday"),
+        Day::Tuesday => println!("Donut day"),
+        Day::Wednesday => println!("Hump day"),
+        Day::Thursday => println!("Pay day"),
+        Day::Friday => println!("Almost Weekend"),
+        Day::Saturday => println!("Weekend"),
+        Day::Sunday => println!("Weekend"),
+    }
+    println!("Is today the weekend :{}", today.is_weekend());
+}
