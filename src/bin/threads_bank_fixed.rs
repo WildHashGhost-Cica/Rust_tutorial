563326606f9391961fef08a314f20d54e606293d
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-10-23 15:32:18 +0100

    threads with bank(fixed)

---
+++ src/bin/threads_bank_fixed.rs
@@ -0,0 +1,54 @@
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufRead, BufReader, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+use std::thread;
+use std::time::Duration;
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::sync::{Arc, Mutex};
+
+fn main(){
+    pub struct Bank {
+        balance: f32
+    }
+    fn withdraw(the_bank: &Arc<Mutex<Bank>>, amt:f32){
+        let mut bank_ref = the_bank.lock().unwrap();
+
+        if bank_ref.balance < 5.00{
+            println!("Current Balance : {} Withdrawal a smaller amount",
+            bank_ref.balance);
+        } else {
+            bank_ref.balance -= amt;
+            println!("Customer withdrew {} Current Balance {}",
+            amt, bank_ref.balance);
+        }
+    }
+
+    fn customer(the_bank: Arc<Mutex<Bank>>) {
+        withdraw(&the_bank, 5.00);
+    }
+
+    let bank: Arc<Mutex<Bank>> =
+      Arc::new(Mutex::new(Bank { balance: 20.00 }));
+
+    // Creates 10 customer threads
+    let handles = (0..10).map(|_| {
+
+        // Clone duplicates an the bank object
+        let bank_ref = bank.clone();
+        thread::spawn(|| {
+            customer(bank_ref)
+        })
+    });
+
+    // Wait for all customers to finish
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+  println!("Total: {}", bank.lock().unwrap().balance);
+
+}
+
